84245b5d375f68676ed50a4bd9fb42430fe69248
Isabellebrian <Isabellebrian@gmail.com>
2023-09-11 21:08:13 +1000

    Initial Commit

---
+++ 2023App/_023AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _023AppApp.swift
+//  2023App
+//
+//  Created by Isabelle Brian on 11/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct _023AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
